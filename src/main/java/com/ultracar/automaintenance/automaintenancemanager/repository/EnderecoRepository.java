91fdf4a08f4dd54c95da82867b233fb84ed46c90
Yago Moreira <yagomoreira@gmail.com>
2024-09-20 16:03:20 -0300

    feat: add repository package

---
+++ src/main/java/com/ultracar/automaintenance/automaintenancemanager/repository/EnderecoRepository.java
@@ -0,0 +1,10 @@
+package com.ultracar.automaintenance.automaintenancemanager.repository;
+
+import com.ultracar.automaintenance.automaintenancemanager.entity.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+
+}
---
+++ src/main/java/com/ultracar/automaintenance/automaintenancemanager/repository/VeiculoRepository.java
@@ -0,0 +1,10 @@
+package com.ultracar.automaintenance.automaintenancemanager.repository;
+
+import com.ultracar.automaintenance.automaintenancemanager.entity.Veiculo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VeiculoRepository extends JpaRepository<Veiculo, Long> {
+
+}
