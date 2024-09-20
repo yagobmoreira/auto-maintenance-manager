9275b93b3df9e690d09c7c0701680b37c26962d9
Yago Moreira <yagomoreira@gmail.com>
2024-09-20 11:34:12 -0300

    first commit

---
+++ src/test/java/com/ultracar/automaintenance/automaintenancemanager/AutoMaintenanceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ultracar.automaintenance.automaintenancemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoMaintenanceManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
